69b302dfc1fddf28ee836bb14c2cb1e37692119e
Dario Oddenino <branch13@gmail.com>
2021-04-23 10:22:37 +0200

    Create the project

---
+++ src/Haskchimp.hs
@@ -0,0 +1,15 @@
+{- |
+Copyright: (c) 2021 Dario Oddenino
+SPDX-License-Identifier: MIT
+Maintainer: Dario Oddenino <branch13@gmail.com>
+
+See README for more info
+-}
+
+module Haskchimp
+       ( someFunc
+       ) where
+
+
+someFunc :: IO ()
+someFunc = putStrLn ("someFunc" :: String)
